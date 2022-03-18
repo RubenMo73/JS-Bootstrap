277a2d067666aa2ee3ae1d8ad795301c9e03107d
DoYouLikePotatos? <92162369+RubenMo73@users.noreply.github.com>
2022-03-18 13:12:00 +0100

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,28 @@
+function creaCampos()
+{
+    var input=document.getElementById("contenedor_campo");
+    var crear_input=document.createElement("input");
+    crear_input.type="text";
+    crear_input.placeholder="Hola";
+    crear_input.onblur=validarInput;
+    input.appendChild(crear_input);
+}
+function validarInput()
+{
+    var aviso=document.getElementById("contenedor_aviso");
+    var inputs=document.getElementsByTagName("input").value;
+    if (inputs.length<4)
+    {
+        var fallo=document.createElement("p");
+        fallo.className="alert alert-danger";
+        fallo.innerHTML="<strong>ERROR!</strong>El campo no tiene 4 carácteres o más";
+        aviso.appendChild(fallo);
+    }
+    else
+    {
+        var correcto=document.createElement("p");
+        correcto.className="alert alert-succes";
+        correcto.innerHTML="<strong>CORRECTO!</strong>El campo está correcto";
+        aviso.appendChild(correcto);
+    }
+}
